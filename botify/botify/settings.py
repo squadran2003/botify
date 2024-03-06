67857db5f6f3d9c43f4253a16f2bcd5332cdd141
Andreas Cormack <andreas@passthekeys.co.uk>
2024-03-06 20:26:57 +0000

    added  custom user model

--- botify/botify/settings.py
+++ botify/botify/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "customusers",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "customusers.User"
--- botify/customusers/models.py
+++ botify/customusers/models.py
@@ -1,10 +1,64 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
-class User(AbstractBaseUser):
+class CustomManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=email,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.admin = True
+        user.staff = True
+        user.superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    address = models.TextField()
-    date_of_birth = models.DateField()
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    objects = CustomManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_superuser(self):
+        return self.superuser
+    @property
+    def is_active(self):
+        return self.active
+
