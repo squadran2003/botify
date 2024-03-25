95e8c817b192920e09fe850fb699db7a5836da78
Andreas Cormack <andreas@passthekeys.co.uk>
2024-03-25 18:51:22 +0000

    added validation to login form

--- botify/customusers/forms.py
+++ botify/customusers/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
-from customusers.models import CustomUser
+from customusers.models import User
+from django import forms
 
 
 class CustomUserForm(ModelForm):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(
+                attrs={'class': 'input', 'placeholder': 'Password'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'class': 'input', 'placeholder': 'Email'}
+            )
+        }
--- botify/customusers/views.py
+++ botify/customusers/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from customusers.forms import CustomUserForm
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'login.html')
+        form = CustomUserForm()
+        return render(request, 'login.html', {'form': form})
 
     def post(self, request):
-        user = authenticate(email=request.POST['email'], password=request.POST['password'])
+        form = CustomUserForm(request.POST)
+        user = authenticate(
+            email=request.POST['email'],
+            password=request.POST['password']
+        )
         if user:
             # handle successful login
             login(request, user)
             return render(request, 'index.html')
         else:
             error = "Invalid email or password"
-            return render(request, 'login.html', {'error': error})
+            return render(request, 'login.html', {'error': error, 'form': form})
 
 
 class LogoutView(View):
