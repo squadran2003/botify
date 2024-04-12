a6973149939768d0eddded1779896e222d698fae
Andreas Cormack <andreas@passthekeys.co.uk>
2024-04-12 18:12:18 +0100

    reformat uploaded date

--- botify/songs/templatetags/custom_filters.py
+++ botify/songs/templatetags/custom_filters.py
@@ -1,9 +1,16 @@
 from django import template
-from django.db.models import FileField
+import datetime
 
 register = template.Library()
 
 
-@register.filter
-def is_file_field(field):
-    return isinstance(field.field, FileField)
+@register.simple_tag
+def convert_date(date):
+    # return date in a humean readable format like 1 hour ago, 1 day ago, 1 month ago, 1 year ago
+    now = datetime.datetime.now().date()
+    date = now - date.date()
+    if date.days == 0:
+        return "Today"
+    elif date.days == 1:
+        return "Yesterday"
+    return "{} days ago".format(date.days)
