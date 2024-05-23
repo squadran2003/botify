b523d37fe03f3e5701db88f6c7f1363beed4a042
Andreas Cormack <andreas@passthekeys.co.uk>
2024-05-23 14:22:25 +0100

    testing ci cd pipeline

--- botify/botify/settings.py
+++ botify/botify/settings.py
@@ -17,7 +17,7 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = True
 
 if DEBUG:
     print(DEBUG)
