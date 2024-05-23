13b087a3d3e65a6f5ae8340756f170d3b146be93
Andreas Cormack <andreas@passthekeys.co.uk>
2024-05-23 12:29:50 +0100

    testing secrets manager

--- botify/botify/production_settings.py
+++ botify/botify/production_settings.py
@@ -1,5 +1,8 @@
 from .settings import *
 from decouple import config, Csv
+import boto3
+from botocore.exceptions import ClientError
+import json
 
 DEBUG = False
 
@@ -13,8 +16,44 @@ CSRF_TRUSTED_ORIGINS = [
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+def get_secret():
+
+    secret_name = "botifyapp-secrets"
+    region_name = "eu-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secrets = get_secret_value_response['SecretString']
+    return json.loads(secrets)
+
+
+SECRETS = get_secret()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': SECRETS["DB_NAME"],
+        'USER': SECRETS["DB_USER"],
+        'PASSWORD': SECRETS["DB_PASSWORD"],
+        'HOST': SECRETS["DB_HOST"],
+        'PORT': '5432',
+    }
+}
+AWS_ACCESS_KEY_ID = SECRETS['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = SECRETS['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = "{}.s3.eu-west-2.amazonaws.com".format(AWS_STORAGE_BUCKET_NAME)
 AWS_S3_OBJECT_PARAMETERS = {
